5188a30cbb21e765100f88d109a012cef96213aa
Peter Bethke <peter@bethke-online.de>
2014-10-27 22:35:05 +0100

    modified:   bin/check_pp.bash

--- bin/check_pp.bash
+++ bin/check_pp.bash
@@ -1,11 +1,15 @@
 #!/bin/bash
+# .pp files testen, mit verschiedene Tests.
+# Peter Bethke
+# 27.10.2014  
 
-lintopt=""
+
+#lintopt="--no-80chars-check"
 
 for i in $*
 do
 	echo "=== Puppet-lint"
-	puppet-lint $i
+	puppet-lint $lintopt $i
 	echo "=== Puppet validate"
 	puppet parser validate $i
 done
